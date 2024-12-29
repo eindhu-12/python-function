0f69cac518e93d7f1e1067e747ea281433dd9e35
unknown <rapoluindhu2929@gmail.com>
2024-12-29 23:51:55 +0530

    Function in python

---
+++ fibnocci.py
@@ -0,0 +1,34 @@
+# -----------------------------factorial--------------------
+def fact(n):
+    if(n==0 or n==1):
+        return n
+    return n*fact(n-1)
+print(fact(5))
+
+print()
+# ------------------------------------------------------
+num1=80
+def fun():
+    # global num1
+    num1=90
+    # global()=95
+    print("Num is:-",num1)
+fun()
+print("output:-",num1)
+
+print()
+
+# ------orbitary arguments-------------
+def sumOf(a,b,*args):
+    res=0
+    res=a+b
+    for i in args:
+        res+=i
+    return res
+print(sumOf(5,6,3,2))
+
+# -------------keyword orbitary arguments-----------
+#pass by value
+#pass by reference
+
+
